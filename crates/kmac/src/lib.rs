7f4bc587496e321a215a42c7aa26fe79cf222da6
Eric Lagergren <eric@ericlagergren.com>
2024-12-18 14:01:59 -0800

    reorganize
    Signed-off-by: Eric Lagergren <eric@ericlagergren.com>

---
+++ crates/kmac/src/lib.rs
@@ -0,0 +1,32 @@
+//! KMAC per NIST [SP 800-185].
+//!
+//! [SP 800-185]: https://nvlpubs.nist.gov/nistpubs/SpecialPublications/NIST.SP.800-185.pdf
+
+#![cfg_attr(docsrs, feature(doc_cfg))]
+#![cfg_attr(not(any(test, doctest, feature = "std")), no_std)]
+#![deny(unused_qualifications)]
+#![deny(clippy::alloc_instead_of_core)]
+#![deny(clippy::cast_lossless)]
+#![deny(clippy::cast_possible_wrap)]
+#![deny(clippy::cast_precision_loss)]
+#![deny(clippy::cast_sign_loss)]
+#![deny(clippy::expect_used)]
+#![deny(clippy::implicit_saturating_sub)]
+#![deny(clippy::indexing_slicing)]
+#![deny(clippy::missing_panics_doc)]
+#![deny(clippy::panic)]
+#![deny(clippy::ptr_as_ptr)]
+#![deny(clippy::string_slice)]
+#![deny(clippy::transmute_ptr_to_ptr)]
+#![deny(clippy::undocumented_unsafe_blocks)]
+#![deny(clippy::unimplemented)]
+#![deny(clippy::unwrap_used)]
+#![deny(clippy::wildcard_imports)]
+#![deny(missing_docs)]
+#![deny(rust_2018_idioms)]
+#![deny(unused_lifetimes)]
+
+mod mac;
+
+pub use generic_array;
+pub use mac::*;
