3a6460c1b98f938a66ad5dafb55f221f9f8edfde
Eric Lagergren <eric@ericlagergren.com>
2024-12-19 14:45:57 -0800

    no-std
    Signed-off-by: Eric Lagergren <eric@ericlagergren.com>

--- crates/utils/src/lib.rs
+++ crates/utils/src/lib.rs
@@ -1,7 +1,7 @@
 //! SHA-3 utilities.
 
 #![cfg_attr(docsrs, feature(doc_cfg))]
-//#![cfg_attr(not(any(test, doctest, feature = "std")), no_std)]
+#![cfg_attr(not(any(test, doctest, feature = "std")), no_std)]
 
 mod enc;
 mod util;
